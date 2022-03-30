0189ff5e84d77a092375720e81ee4e12cb14b355
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-30 21:22:58 +0530

    Arithmatic

---
+++ C Assignment/Subtraction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int num1,num2,res;
+	printf("enter 2 int number");
+	scanf("%d %d",&num1,&num2);
+	
+	res= num1-num2;
+	
+	printf("the subtracted value=%d",res);
+	getch();
+	
+}
