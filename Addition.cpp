db27b179ae383ae5ac6a1bf92286106d76dbaa8e
NAHIDANJUM22 <102017242+NAHIDANJUM22@users.noreply.github.com>
2022-03-30 16:51:23 +0530

    Addition

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+
+int num1,num2,res;
+printf("enter 2 int num");
+scanf("%d %d", &num1,&num2);
+res= num1+num2;
+printf("the add res=%d",res);
+
+getch();
+}
