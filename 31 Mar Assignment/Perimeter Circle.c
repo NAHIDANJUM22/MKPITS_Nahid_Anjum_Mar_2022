b082b57b70097e753e5f1a16ad5369dc498b841b
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:47:36 +0530

    peri circle

---
+++ 31 Mar Assignment/Perimeter Circle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	
+	float pi,perimeter;
+	int radi;
+	
+	printf("enter the value of radius");
+	scanf("%d",&radi);
+	
+	perimeter= 2*3.142*radi;
+	
+	printf("the perimeter of circle=%f",perimeter);
+}
