a8ddc40763892ed3ad512b383617f5101a423a2f
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:45:59 +0530

    peri square

---
+++ 31 Mar Assignment/Perimeter Square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int side,perimeter;
+	printf("enter the side value of square");
+	scanf("%d",&side);
+	
+	perimeter=4*side;
+	
+	printf("the perimeter of squre=%d",perimeter);
+}
