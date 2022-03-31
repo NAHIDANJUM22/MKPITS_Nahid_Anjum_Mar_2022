ca9af96952c8e7b5c13af46eabd1a6a4783a47c6
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:53:21 +0530

    area of triangle

---
+++ 31 Mar Assignment/Triangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	
+	int base,height;
+	float area;
+	
+	
+	printf("enter the values");
+	scanf("%d %d", &base,&height);
+	
+	area = base*height*0.5;
+	
+	printf("area of triangle is =%f",area);
+	
+}
