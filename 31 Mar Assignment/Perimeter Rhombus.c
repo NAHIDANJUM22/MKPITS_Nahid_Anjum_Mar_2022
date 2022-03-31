71559498a436a95dd22b683884c6f677c53c8fda
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:34:24 +0530

    peri rhombus

---
+++ 31 Mar Assignment/Perimeter Rhombus.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a,perimeter;
+	
+	printf("enter the value of a");
+	scanf("%d",&a);
+	
+	perimeter=4*a;
+	
+	printf("the perimeter of rhombus=%d",perimeter);
+		
+}
