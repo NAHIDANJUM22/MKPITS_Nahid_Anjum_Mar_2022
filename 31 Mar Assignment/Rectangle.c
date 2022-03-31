69e84cf230ca71ef91e3da54add51bfaf1fb46eb
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:54:45 +0530

    area of rectangle

---
+++ 31 Mar Assignment/Rectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	
+	
+	int width,height,area;
+	
+	printf("enter the width and height");
+	scanf("%d %d" ,&width,&height);
+	
+	area=width*height;
+	
+	printf("area of rectangle=%d",area);
+}
