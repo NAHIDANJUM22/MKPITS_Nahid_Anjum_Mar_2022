c0a537364612b017ef3747f978dcd84e1d12f1ca
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:43:18 +0530

    parallelogram

---
+++ 31 Mar Assignment/Parallelogram.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int side,height,area;
+	
+	printf("enter the value of side and height");
+	scanf("%d %d",&side,&height);
+	
+	area=side*height;
+	
+	printf("the area of parallelogram=%d",area);
+	
+	
+	
+}
