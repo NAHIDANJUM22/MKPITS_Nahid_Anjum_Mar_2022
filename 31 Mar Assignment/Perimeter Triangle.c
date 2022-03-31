46b4a91de23e79a47c15ae423ba1df7e8cc50014
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:49:13 +0530

    peri triangle

---
+++ 31 Mar Assignment/Perimeter Triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+int num1,num2,num3,perimeter;
+
+printf("enter the values");
+scanf("%d %d %d",&num1,&num2,&num3);
+
+perimeter= num1+num2+num3;
+
+printf("perimeter of triangle=%d",perimeter);
+
+}
