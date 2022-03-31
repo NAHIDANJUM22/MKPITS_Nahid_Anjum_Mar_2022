cc7fbad9610ff9fb3076fdfd50ef4a02b461b432
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:28:01 +0530

    peri cube

---
+++ 31 Mar Assignment/Perimeter Cube.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int side,perimeter;
+	
+	printf("the value of side");
+	scanf("%d",&side);
+	
+	perimeter=12*side;
+	
+	printf("perimeter of cube=%d",perimeter);
+}
