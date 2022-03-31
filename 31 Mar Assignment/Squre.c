d872571a350b5c56cec4075d1c85b7f41fc96b41
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:51:46 +0530

    area of square

---
+++ 31 Mar Assignment/Squre.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int side,area;
+	
+	printf("enter the side value");
+	scanf("%d",&side);
+	
+	area=side*side;
+	
+	printf("the area of squre =%d",area);
+}
