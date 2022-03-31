6324b1fa3282ba6938f7f63f7a670d4ae2b4dd7a
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-03-31 22:31:36 +0530

    peri trapezium

---
+++ 31 Mar Assignment/Perimeter Trapezium.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+	int a,b,c,d,perimeter;
+	
+	printf("enter the value of a,b,c and d");
+	scanf("%d %d %d %d",&a,&b,&c,&d);
+	
+	perimeter=a+b+c+d;
+	
+	printf("the perimeter of trapezium=%d",perimeter);
+}
