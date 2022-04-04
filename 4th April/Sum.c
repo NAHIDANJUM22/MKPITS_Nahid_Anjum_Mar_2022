261b82eaeadb0e4dad86cea2a12907c0fe4214cb
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-04 19:35:07 +0530

    Addition of digit

---
+++ 4th April/Sum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+    int num,n1,n2,n3,sum;
+    
+	printf("enter three digit number \n");
+	scanf("%d",&num);
+	
+	n1=num%10;
+	num=num/10;
+	
+	n2=num%10;
+	num=num/10;
+	
+	n3=num%10;
+	
+	sum=n1+n2+n3;
+	
+	printf("addition of digit=%d",sum);
+}
