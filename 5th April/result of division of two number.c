d19e0970eef4d2143bd63394357858f69000896c
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-05 23:01:12 +0530

    division of 2 no

---
+++ 5th April/result of division of two number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a,b,res;
+	
+	printf("enter 2 number \n");
+	scanf("%d%d",&a,&b);
+	
+	res=a/b;
+	
+	printf("result of dividing 2 numbers=%d",res);
+	
+}
