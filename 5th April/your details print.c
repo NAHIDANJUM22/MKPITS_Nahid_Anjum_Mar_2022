04423e03e73e893be46f85255116be55aad1a5e0
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-06 00:58:55 +0530

    print your details

--- 5th April/your details print.c
+++ 5th April/your details print.c
@@ -2,12 +2,7 @@
 #include<conio.h>
 main()
 {
-	char name;
-	int dob,mob;
-	
-	printf("enter the details \n");
-	
-	printf("name=Nahid \n",name);
-	printf("dob=January 07,1993 \n",dob);
-	printf("mob no=9009349369 \n",mob);
+	printf("name=Nahid \n");
+	printf("dob=January 07,1993 \n");
+	printf("mob no=9009349369");
 }
