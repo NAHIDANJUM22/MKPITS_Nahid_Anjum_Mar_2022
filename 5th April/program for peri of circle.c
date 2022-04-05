5507742b02113e502ccde6daaa98cd98e6d0b6de
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-05 22:20:35 +0530

    program for perimeter of circle

---
+++ 5th April/program for peri of circle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+int r;
+float pi,peri;
+
+printf("enter the radius \n");
+scanf("%d",&r);
+
+peri=2*3.14*r;
+
+printf("the perimeter of circle=%f",peri);
+
+}
