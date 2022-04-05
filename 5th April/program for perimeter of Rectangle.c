8a07e554d7093ed5f05c376e2c233a3fc3ee364b
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-05 22:17:18 +0530

    program for perimeter of rectangle

---
+++ 5th April/program for perimeter of Rectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+int a,b,peri;
+
+printf("enter the value \n");
+scanf("%d%d",&a,&b);
+
+peri=2*(a+b);
+
+printf("the perimeter of rectangle=%d",peri);
+
+}
