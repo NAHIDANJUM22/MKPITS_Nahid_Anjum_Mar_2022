5b8d4b9f4d1604a686e8283a145842b9dd9a61eb
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-05 22:08:13 +0530

    program for area of rectangle

---
+++ 5th April/program for area of Rectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+int a,b,area;
+
+printf("enter the value \n");
+scanf("%d%d",&a,&b);
+
+area=a*b;
+
+printf("the area of rectangle=%d",area);
+
+}
