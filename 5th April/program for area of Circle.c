6023779f9b609ad7d04dc9db55801fc0c1b4a8f1
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-05 22:18:58 +0530

    program for area of circle

---
+++ 5th April/program for area of Circle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+int r;
+float pi,area;
+
+printf("enter the radius \n");
+scanf("%d",&r);
+
+area=3.14*r*r;
+
+printf("the area of circle=%f",area);
+
+}
