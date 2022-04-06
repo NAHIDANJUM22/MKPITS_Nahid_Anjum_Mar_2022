13ea04f2570f4c839d16235444901fd15fce2c7c
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-06 16:44:52 +0530

    print F and C

---
+++ 6th April/F and C print.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	printf(" #####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	
+	printf("       #########\n");
+	printf("    ##          ##\n");
+	printf("    #\n");
+	printf("    #\n");
+	printf("    #\n");
+	printf("    #\n");	
+	printf("    ##          ##\n");
+	printf("       #########\n");
+}
