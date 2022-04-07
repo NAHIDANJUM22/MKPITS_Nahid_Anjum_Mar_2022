c7a04fe4ab2494a77e7fa45d7e39848e0263b08a
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-07 14:07:21 +0530

    distance

---
+++ 6th April/distance.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a,b,distance;
+	printf("enter 2 points \n");
+	scanf("%d%d",&a,&b);
+	
+	distance=a-b;
+	
+	printf("distance between 2 points =%d",distance);
+}
