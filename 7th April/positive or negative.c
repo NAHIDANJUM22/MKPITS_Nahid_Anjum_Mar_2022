52086abf5bfd200b7ed71ea9d930f50e7673189b
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-07 19:14:27 +0530

    positive or negative

---
+++ 7th April/positive or negative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num;
+	printf("enter number \n");
+	scanf("%d",&num);
+	
+	if(num>0)
+	{
+		printf("%d number is positive",num);
+		
+	}
+	else
+	{
+		printf("%d number is negative",num);
+	}
+}
