fcec824979f8158c6330f4ef75861b1b7e8b202b
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-09 00:08:44 +0530

    distance bet 2 points

--- 6th April/distance.c
+++ 6th April/distance.c
@@ -3,11 +3,21 @@
 
 main()
 {
-	int a,b,distance;
-	printf("enter 2 points \n");
-	scanf("%d%d",&a,&b);
+	int a1,b1,a2,b2,dis;
+	printf("enter a1\n");
+	scanf("%d",&a1);
+	printf("enter b1\n");
+	scanf("%d",&b1);
 	
-	distance=a-b;
+	printf("enter a2\n");
+	scanf("%d",&a2);
+	printf("enter b2\n");
+	scanf("%d",&b2);
 	
-	printf("distance between 2 points =%d",distance);
+	
+	dis=sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+	
+	printf("distance between 2 points is %d",dis);
 }
+
+
