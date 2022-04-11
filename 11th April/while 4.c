bf937ebcc0733f607e7aa03e767133b96ff3e29f
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-11 16:53:52 +0530

    class program 11th april

---
+++ 11th April/while 4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i;
+	i=0;
+	
+	while(i<=10)
+	{
+		
+		printf("%d",i);
+		i=i+2;
+		
+	}
+}
---
+++ 11th April/while5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,value;
+	i=1;
+	value=2;
+	
+	while(i<=10)
+	{
+		printf("%d",i);
+		i=i+2;
+		
+	}
+}
