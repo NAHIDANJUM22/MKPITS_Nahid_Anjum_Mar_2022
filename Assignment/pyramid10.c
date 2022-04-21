9f105c5b2b361d292d71552429bc748c84e0470d
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-21 14:03:42 +0530

    pattern10

---
+++ Assignment/pyramid10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int r,c;
+	for(r=1;r<=3;r++)
+	{
+	
+		for(c=3;c>r;c--)
+		{
+		printf(" ");
+		}
+		for(c=1;c<=r;c++)
+		{
+		printf("%d",c);
+		}
+		printf("\n");
+	}
+}
