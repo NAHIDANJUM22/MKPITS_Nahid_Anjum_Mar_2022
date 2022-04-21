f52530d7efa5e17df9f0f6c7c2d173e003489f42
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-21 21:10:15 +0530

    pattern11

---
+++ Assignment/pyramid11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int r,c;
+	for(r=1;r<=4;r++)
+	{
+
+		for(c=1;c<=5;c++)
+		{
+			if(r%2==0)
+			printf("a");
+			else
+			printf("%d",c);
+		}
+		
+		printf("\n");
+	}
+}
