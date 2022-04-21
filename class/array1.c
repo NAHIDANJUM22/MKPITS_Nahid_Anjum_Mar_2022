0320149a8027f25bebad9ea9f8c4617f02f7c741
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-21 21:15:26 +0530

    array1

---
+++ class/array1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int i,a[2];
+ 
+ printf("enter 3 no\n");
+ for(i=0;i<=2;i++)
+ {
+ 	printf("a[%d]= ",i);
+ 	scanf("%d",&a[i]);
+ }
+ for(i=0;i<=2;i++)
+ {
+ 	printf("a[%d]=%d",i,a[i]);
+ }
+ 
+ 
+}
