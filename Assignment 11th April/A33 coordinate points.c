04b176cb6f7c55b5fa91527b42952b6259c59a8f
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-23 00:07:11 +0530

    A33 coordinate points

--- Assignment 11th April/A33 coordinate points.c
+++ Assignment 11th April/A33 coordinate points.c
@@ -4,39 +4,28 @@ main()
 {
 	int x,y;
 	
-    printf("enter the values of X and Y coordinate\n");
+    printf("enter the values of X and Y \n");
 	scanf("%d%d",&x,&y);
 
 	if(x>0 && y>0)
 	{
-	  printf("the coordinate points lies on the 1st quandrant%d%d\n",&x,&y);
+	  printf("the coordinate points lies on the 1st quandrant\n");
 		
 	}
 	else
 	if(x<0 && y>0)
 	{
-		printf("the coordinate points lies on the 2nd quandrant%d%d\n",&x,&y);
+		printf("the coordinate points lies on the 2nd quandrant\n");
 	}
 	else
 	if(x<0 && y<0)
 	{
-		printf("the coordinate points lies on the 3rd quandrant%d%d\n",&x,&y);
+		printf("the coordinate points lies on the 3rd quandrant\n");
 		
 	}
 	else
-	if(x>0 && y<0)
 	{
-	printf("the coordinate points lies on the 4th quandrant%d%d\n",&x,&y);
+	printf("the coordinate points lies on the 4th quandrant");
 	}
-	else
-	if(x==0 && y==0)
-	{
-	printf("the coordinate points lies at the origin%d%d\n",&x,&y);
-	}
-	else
-	{
-	printf("invalid");
-	}
-	
 			
 }
