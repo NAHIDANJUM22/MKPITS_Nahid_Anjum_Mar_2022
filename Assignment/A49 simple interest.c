56abe9b1e73a536feac632845b05a9a249534222
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-23 23:31:29 +0530

    A49 simple interest

---
+++ Assignment/A49 simple interest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int p,r,t,a;
+	printf("calculate simple interest\n");
+	
+	printf("enter principal\n");
+	scanf("%d",&p);
+	printf("enter rate of interest\n");
+	scanf("%d",&r);
+	printf("enter time\n");
+	scanf("%d",&t);
+	
+	a=(p*r*t)/100;
+	
+	printf("Simple Interest =%d",a);
+}
