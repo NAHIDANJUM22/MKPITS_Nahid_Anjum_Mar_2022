48f61de9e10004e76e3e06b2f148cbf7b7c595d4
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-27 01:07:18 +0530

    A53 reverse a given no

---
+++ Assignment/A53 reverse a given no.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n,m,rev=0;
+	printf("enter number");
+	scanf("%d",&n);
+	
+	while(n!=0)
+	{
+		m=n%10,
+	    n=n/10;
+	
+	    rev=rev*10+m;	
+	}
+	 printf("rev=%d",rev);	
+}
