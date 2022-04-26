544176079cb05ccb45c813206e08a9f381349d1f
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-26 21:37:07 +0530

    modified 41,42 assignment

---
+++ Assignment/A41 .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	float s=0,i;
+	
+
+   for(i=1;i<=50;i++)
+   {
+	s=s+1/i;
+   }
+ printf("%f",s);
+}
--- Assignment/A42 calculate fraction.c
+++ Assignment/A42 calculate fraction.c
@@ -2,14 +2,15 @@
 #include<conio.h>
 main()
 {
-	float s=0,j=1,a,i;
+	float s=0,j=1,i;
 	printf("value of s \n where s=1+3/2+5/4+7/8");
 	for(i=0;i<=7;i+2)
 	{
-	 a=i/j;
-	 s=s+a;
+	 s=s+i/j;
+	 
+	 i=i+2;
 	 j=j*2;
 	}
-	printf("value of s %f",s);
+	printf("value of s %f\n",s);
 }
 	
