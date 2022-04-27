fd8339a3cbcba8d9fefd70e9886ed4b9d69a1c9f
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-27 20:25:24 +0530

    A62 alphabet set in decimal and character form

---
+++ Assignment/A62 alphabet set in decimal and char form.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	char c;
+	printf("capital alphabet set in decimal\n");
+	for(c='65';c<=90;c++)
+	{
+		printf("%c=%d\n",c,c);
+	}
+		printf("small alphabet set in decimal\n");
+		for(c='97';c<=122;c++)
+	{
+		printf("%c=%d\n",c,c);
+	}
+}
+
+
