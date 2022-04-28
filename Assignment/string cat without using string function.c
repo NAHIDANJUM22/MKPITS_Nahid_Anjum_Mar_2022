36bb84bbbc1cb8d6dbb59848b173dd6a4d89a64b
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-28 13:51:44 +0530

    stringcat without using string function

---
+++ Assignment/string cat without using string function.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	char n1[15],n2[15];
+	int i,j;
+	printf("enter 1st name\n");
+	scanf("%s",n1);
+	printf("enter 2nd name\n");
+	scanf("%s",n2);
+	for(i=0;n1[i]!='\0';i++)
+	{
+	for(j=0;n2[i]!='\0';j++)
+	{
+		n1[i]=n2[j];
+	}
+}
+
+	    n1[i]='\0';
+	
+	    printf("%s",n1);
+	
+
+	
+}
