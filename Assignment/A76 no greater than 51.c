aaa53ecd89607ca08cba2efb4c362c440f6164d7
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-29 13:48:58 +0530

    A76 no greater than 51

---
+++ Assignment/A76 no greater than 51.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num,n1=51,r;
+	printf("enter no");
+	scanf("%d",&num);
+	
+	if(num>n1)
+	{
+		r=(n1-num)*3;
+		printf("result=%d",r);
+	}
+	else
+	{
+		r=n1-num;
+		printf("result=%d",r);
+	}
+}
