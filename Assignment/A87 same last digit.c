ef3b0063c61bbcb173e6c6952ee8f197cd50c625
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-04-30 16:28:11 +0530

    A87 same last digit

---
+++ Assignment/A87 same last digit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num1,num2;
+	printf("enter two nos\n");
+	scanf("%d%d",&num1,&num2);
+	
+	if(num1%10==num2%10)
+	{
+		printf("same last digit\n");
+	}
+	else
+	{
+		printf("different last digit\n");
+	}
+}
+
