dbcdfe2dca999ac9f8daad3d8ac5c7e0b5e1062c
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-01 21:20:38 +0530

    string cat without using string function

--- Assignment/string cat without using string function.c
+++ Assignment/string cat without using string function.c
@@ -8,18 +8,15 @@ main()
 	scanf("%s",n1);
 	printf("enter 2nd name\n");
 	scanf("%s",n2);
+	
 	for(i=0;n1[i]!='\0';i++)
 	{
-	for(j=0;n2[i]!='\0';j++)
+	for(j=0;n2[i]!='\0';j++,i++)
 	{
 		n1[i]=n2[j];
 	}
-}
-
-	    n1[i]='\0';
-	
-	    printf("%s",n1);
-	
-
-	
+        n1[i]='\0';
+     printf("%s",n1);
+    }
+    
 }
