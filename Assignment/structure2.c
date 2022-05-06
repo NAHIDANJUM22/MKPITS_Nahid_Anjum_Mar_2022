58cb375ebb0b87b46fdf7edcf838cfd616b436f2
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-06 17:25:10 +0530

    structure2

---
+++ Assignment/structure2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+ struct student        //structure student name created
+{
+	int id;
+	char name [15];
+} ;                 
+void main()
+{
+	struct student s1;
+	
+	printf("enter student id\n");
+	scanf("%d",&s1.id);
+	printf("enter student name\n");
+	scanf("%s",s1.name);
+	
+	printf("studend id =%d\n",s1.id);
+	printf("student name=%s",s1.name);
+}
