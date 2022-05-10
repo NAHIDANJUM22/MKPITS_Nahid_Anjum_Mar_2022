176b16cad78160c9ff4c69976a57047bace8432c
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-10 16:58:55 +0530

    A107 integer contain 15 or 20

---
+++ Assignment/A107 integer contain 15 or 20.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,a[2],p=0;
+	for(i=0;i<2;i++)
+	{
+     printf("enter array element\n");
+     scanf("%d",&a[i]);
+	}
+
+	for(i=0;i<2;i++)
+	{
+		if(a[i]==15 || a[i]==20)
+		{
+			printf("true");
+			p=1;
+			break;
+		}
+	}
+		if(p==0)
+		{
+		    printf("false");	
+		}
+}
