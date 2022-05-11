a11f12e9592275d6d1ebf845c1cf515f972ebf63
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-11 22:33:11 +0530

    nested for loop 299

---
+++ Assignment/nested for loop 299.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int r,c;
+	for(r=1;r<=3;r++)
+	{
+		for(c=3;c>r;c--)
+		{
+			printf("  ");
+		}
+	for(c=1;c<=r;c++)
+	{
+		printf("* ");
+	}
+		printf("\n");
+	}
+}
