f569f79ff8c1b0ded736182329db5889745dda2e
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-18 17:58:03 +0530

    A132

---
+++ Assignment/A132 same elements appears at the start and end of given array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n,i,l,f=0;
+	printf("enter size of array");
+	scanf("%d",&n);
+	printf("enter length");
+	scanf("%d",&l);
+	int a[n];
+	printf("enter element of array\n");
+	for(i=0;i<l;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<l;i++)
+	{
+		if(a[i]!=a[n - l + i])
+		{
+			f=0;
+		}
+	}
+	if(f==1)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
