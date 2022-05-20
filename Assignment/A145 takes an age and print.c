af9854748be722023380905e52f0287e5eb9309b
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-21 00:49:55 +0530

    A145

---
+++ Assignment/A145 takes an age and print.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int age;
+	
+	printf("enter age\n");
+	scanf("%d",&age);
+	if(age>20)
+	{
+		printf("you look older than 20");
+	}
+	else
+	{
+	    printf("you look younger than 20");	
+	}
+}
