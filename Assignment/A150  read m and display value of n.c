ba397fb13853819837379eeb363eb0cecc403f6b
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-21 00:53:45 +0530

    A150

---
+++ Assignment/A150  read m and display value of n.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int m,n;
+	printf("Enter age of the candidate\n");
+	scanf("%d",&m);
+	
+	if(m>0)
+	{
+	   	printf("n=1");	
+	}
+	else if(m==0)
+	{
+		printf("n=0");	
+	}
+	else
+	{
+		printf("n=-1");
+	}
+}
