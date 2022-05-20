df5d4169bb85bc305cea219b8b20da1dbd0866ec
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-21 00:50:38 +0530

    A146

---
+++ Assignment/A146 display no in rectangle of 3 column wide and 5 rows tall.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num;
+	printf("Enter a number you want to Print\n");
+	scanf("%d",&num);
+	printf("\n");
+	printf("Rectangle Of 3 Column and 5 Row\n");
+	printf(" %d %d %d \n",num,num,num);
+	printf(" %d   %d \n",num,num);
+	printf(" %d   %d \n",num,num);
+	printf(" %d   %d \n",num,num);
+	printf(" %d %d %d \n",num,num,num);
+}
