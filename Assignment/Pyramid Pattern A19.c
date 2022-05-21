1244a8466d6a42388ff973978a5266e9aad244ba
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-05-21 23:06:05 +0530

    Pyramid Pattern A19

---
+++ Assignment/Pyramid Pattern A19.c
@@ -0,0 +1,39 @@
+//   A B C D E F G
+//     A B C D E   
+//       A B C
+//         A
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        int i,j,sp,n='A';
+        for(i=4;i>=1;i--)
+        {
+            for(sp=1;sp<=41-i;sp++)
+            {
+                printf(" ");
+            }
+            for(j=4;j>=1;j--)
+            {
+                if(j<=i)
+                {
+                  printf("%c ",n);
+                  n++;
+				}
+				else
+				{
+					printf(" ");
+				}
+            }
+            for(j=1;j<=4;j++)
+            {
+                   if(j<i)
+                {
+                  printf("%c ",n);
+                  n++;
+				}
+			}
+			n='A';
+			printf("\n");
+        }
+}
