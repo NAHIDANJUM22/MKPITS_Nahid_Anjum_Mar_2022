2174bdac7e67f0f6437be29f9973671a0e129a51
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-09-23 02:17:39 +0530

    first query

---
+++ SQL Server/First_Program.sql
@@ -0,0 +1,6 @@
+create table sample(id int)
+create table student(rno int,name varchar(20))
+insert into student values(1,'amit')
+select * from student
+insert into student values(2,'mayuri')
+select * from student
