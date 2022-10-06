761a24dcac6813fe1a73d6c5275ce99c2a3651dc
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-10-06 19:59:59 +0530

    second program

---
+++ SQL Server/Second Program.sql
@@ -0,0 +1,7 @@
+create table product (proid int,proname varchar(30),price int,quantity int)
+insert into product values(1,'soap',50,20) 
+select * from product
+insert into product values(2,'keyboard',220,7)
+select * from product
+insert into product values(3,'mouse',500,6)
+select * from product
