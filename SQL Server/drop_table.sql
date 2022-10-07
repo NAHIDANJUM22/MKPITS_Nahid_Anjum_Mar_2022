034c4581cea4c3d3da1aa7a09e66cb4f11f0e5ec
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-10-07 16:22:04 +0530

    drop_table

---
+++ SQL Server/drop_table.sql
@@ -0,0 +1,7 @@
+create table customer81(custid int UNIQUE,name varchar(30)UNIQUE)
+insert into customer81 values(102,'amit')
+insert into customer81 values(103,'sumit')
+insert into customer81 values(104,'mita')
+insert into customer81 values(null,null)
+select * from customer81
+drop table customer81
