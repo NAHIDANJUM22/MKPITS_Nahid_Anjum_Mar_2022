fa2e49aa938ecf8d0b00dfb828e16af6a7b3f86e
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-10-07 16:54:26 +0530

    truncate

---
+++ SQL Server/truncate.sql
@@ -0,0 +1,7 @@
+create table student3(id int,name varchar(20),age tinyint)
+insert into student3 values(56,'raghav',45)
+insert into student3 values(57,'raghave',47)
+insert into student3 values(58,'raghavi',49)
+select * from student3
+--to delete all rows
+truncate table student3
