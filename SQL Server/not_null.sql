dc95dbbb803880f092bf53bf574f94443fbc0c74
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-10-07 16:24:05 +0530

    not_null

---
+++ SQL Server/not_null.sql
@@ -0,0 +1,7 @@
+create table pro21(proid int not null,proname varchar(30)not null)
+--to use not null constraints
+insert into pro21 values(85,'key')
+insert into pro21 values(85,'key')
+insert into pro21 values(85,'key')
+
+select * from pro21
