8a12efbeb9833b7cf1e7999b05e0da68111a52a7
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-10-07 16:55:41 +0530

    drop_table1

---
+++ SQL Server/drop_table1.sql
@@ -0,0 +1,7 @@
+create table stud2(rn int,name varchar(30),age int)
+insert into stud2 values(2,'haris',23)
+insert into stud2 values(3,'haris',24)
+insert into stud2 values(4,'haris',25)
+select * from stud2
+--to delete table
+drop table stud2
