56c4b304785e235f920d2a982b63b1ffc8cef569
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-10-17 13:24:46 +0530

    1st normalization

---
+++ SQL Server/1st_normal_form.sql
@@ -0,0 +1,10 @@
+create table employee13(emp_id int,emp_name varchar(30),emp_phone money,emp_state varchar(20))
+--1st normal form
+insert into employee13 values(14,'john',7272826385,'UP')
+insert into employee13 values(14,'john',9064738238 ,'UP')
+insert into employee13 values(20,'harry',7272826385,'Bihar')
+insert into employee13 values(12,'sam',7390372389 ,'Punjab')
+insert into employee13 values(12,'sam',8589830302 ,'Punjab')
+select * from employee13
+
+
