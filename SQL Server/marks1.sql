7c912ed82101ce9493423ed0e1bec16eb9a19516
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-10-17 14:15:26 +0530

    marks

---
+++ SQL Server/marks1.sql
@@ -0,0 +1,6 @@
+Create table marks(phy int,chem int,bio int,total int)
+insert marks values(45,65,69,300)
+insert marks values(75,45,89,300)
+insert marks values(55,75,79,300)
+insert marks values(85,55,59,300)
+select * from marks
