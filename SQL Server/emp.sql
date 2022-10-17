9519e4bb6f5822917f4dce7a07cd967a47d09ea2
NAHIDANJUM22 <anjumnahid425@gmail.com>
2022-10-17 14:13:47 +0530

    employee

---
+++ SQL Server/emp.sql
@@ -0,0 +1,10 @@
+create  table employee8(id int,name varchar(20),age int)
+insert employee8 values(10,'kavita',35)
+insert employee8 values(20,'sunita',45)
+insert employee8 values(30,'pavita',25)
+
+create  table employee88(id int,name varchar(20),age int,addr varchar(30))
+insert employee88 values(10,'kavita',35,'nagpur')
+insert employee88 values(20,'sunita',45,'mumbai')
+insert employee88 values(30,'pavita',25,'nasik')
+
---
+++ SQL Server/empl.sql
@@ -0,0 +1,16 @@
+create table customer71(cid int,cname varchar(30),caddr varchar(40))
+insert into customer71 values(90,'suman','nagpur')
+insert into customer71 values(80,'sumani','mumbai')
+insert into customer71 values(70,'sumane','pune')
+insert into customer71 values(60,'sumano','nasik')
+insert into customer71 values(50,'sumanu','mumbra')
+select * from customer71
+
+create table customer81(cid int,cname varchar(30),caddr varchar(40),cage int)
+insert into customer81 values(90,'suman','nagpur',30)
+insert into customer81 values(80,'sumani','mumbai',35)
+insert into customer81 values(70,'sumane','pune',40)
+insert into customer81 values(60,'sumano','nasik',45)
+insert into customer81 values(50,'sumanu','mumbra',50)
+select * from customer81
+
